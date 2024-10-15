6e71275036d3c11827b23d4cfa77ec6a6ce58b27
석민영 <149374648+MINYOUNG-SEOK@users.noreply.github.com>
2024-10-15 17:58:59 +0900

    Update task.controller.js

--- controller/task.controller.js
+++ controller/task.controller.js
@@ -4,7 +4,7 @@ const taskController = {};
 
 taskController.createTask = async (req, res) => {
   try {
-    const { task, isComplete } = req.body;
+    const { task, isComplete, priority  } = req.body;
     const { userId } = req;
     const newTask = new Task({ task, isComplete, author: userId });
     await newTask.save();
@@ -16,12 +16,15 @@ taskController.createTask = async (req, res) => {
 
 taskController.getTask = async (req, res) => {
   try {
-    const taskList = await Task.find({}).populate("author");
+    const { priority } = req.query; 
+    const filter = priority ? { priority } : {}; 
+    const taskList = await Task.find(filter).populate("author"); 
     res.status(200).json({ status: "ok", data: taskList });
   } catch (err) {
     res.status(400).json({ status: "fail", error: err });
   }
 };
+
 
 taskController.updateTask = async (req, res) => {
   try {
